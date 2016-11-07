5d4a9a0eb7a917d29e416a55c9421170d7032d3f
Xinyuan Liu <lxy_pku@qq.com>
2016-11-07 17:01:35 +0800

    commit

--- simulator.cpp
+++ simulator.cpp
@@ -10,7 +10,7 @@
 #include <sys/time.h>
 using namespace std;
 
-bool verbose=false;
+const bool verbose=false;
 
 const char * RegName [32]={"zero","ra","sp","gp","tp","t0","t1","t2","s0","s1","a0","a1","a2","a3","a4","a5","a6","a7","s2","s3","s4","s5","s6","s7","s8","s9","s10","s11","t3","t4","t5","t6"};
 const char * fRegName [32]={"ft0","ft1","ft2","ft3","ft4","ft5","ft6","ft7","fs0","fs1","fa0","fa1","fa2","fa3","fa4","fa5","fa6","fa7","fs2","fs3","fs4","fs5","fs6","fs7","fs8","fs9","fs10","fs11","ft8","ft9","ft10","ft11",};
@@ -267,7 +267,7 @@ int main(int argc, char ** argv)
     {
         instruction instr=mem.ReadWord(PC);
         PC_next=PC+4;
-        if(verbose)printf("%x:\t%08x\t\t", (uint32_t)PC, instr.code);
+        if(verbose) printf("%x:\t%08x\t\t", (uint32_t)PC, instr.code);
         switch(instr.opcode())
         {
             
